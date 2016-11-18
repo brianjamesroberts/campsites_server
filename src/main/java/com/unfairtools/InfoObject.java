4135495c58e85f26e6b8b48c2126ced02942ec23
brianroberts <robert95@students.wwu.edu>
2016-11-17 20:27:20 -0800

    push(laptop)

--- src/main/java/com/unfairtools/InfoObject.java
+++ src/main/java/com/unfairtools/InfoObject.java
@@ -6,20 +6,21 @@ package com.unfairtools;
 public class InfoObject {
 
 
-
     public String name;
     public double latNorth;
     public double latSouth;
     public double longWest;
     public double longEast;
-    public float latPoint;
-    public float longPoint;
+    public double latPoint;
+    public double longPoint;
 
     public int[] ids;
     public double[] latitudes;
     public double[] longitudes;
     public String[] names;
     public int[] types;
+
+    public String address;
 
 
 
